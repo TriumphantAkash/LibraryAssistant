678875ecede0766abe1ebb13fada1dac60b63cfe
Akash Chaturvedi <p.akashchaturvedi@gmail.com>
2016-08-20 01:37:16 -0500

    added activities based on initial design

---
+++ app/src/main/java/triumphantakash/github/io/librarymanager/BookDetailsActivity.java
@@ -0,0 +1,13 @@
+package triumphantakash.github.io.librarymanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BookDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_details);
+    }
+}
