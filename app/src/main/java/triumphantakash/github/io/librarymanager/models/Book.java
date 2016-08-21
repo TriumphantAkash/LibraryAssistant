5782aef379f6b8a54ff56f9c94a06c03ec30990b
Akash Chaturvedi <p.akashchaturvedi@gmail.com>
2016-08-20 22:59:23 -0500

    Book model updated

--- app/src/main/java/triumphantakash/github/io/librarymanager/models/Book.java
+++ app/src/main/java/triumphantakash/github/io/librarymanager/models/Book.java
@@ -8,6 +8,51 @@ import java.io.Serializable;
 public class Book implements Serializable{
     String bookTitle;
     String bookAuthor;
+    String bookPublisher;
+    String bookCatagories;
+    String bookURL;
+    String lastCheckedOut;
+    String lastCheckedOutBy;
+
+    public void setBookPublisher(String bookPublisher) {
+        this.bookPublisher = bookPublisher;
+    }
+
+    public void setBookCatagories(String bookCatagories) {
+        this.bookCatagories = bookCatagories;
+    }
+
+    public void setBookURL(String bookURL) {
+        this.bookURL = bookURL;
+    }
+
+    public void setLastCheckedOut(String lastCheckedOut) {
+        this.lastCheckedOut = lastCheckedOut;
+    }
+
+    public void setLastCheckedOutBy(String lastCheckedOutBy) {
+        this.lastCheckedOutBy = lastCheckedOutBy;
+    }
+
+    public String getBookPublisher() {
+        return bookPublisher;
+    }
+
+    public String getBookCatagories() {
+        return bookCatagories;
+    }
+
+    public String getBookURL() {
+        return bookURL;
+    }
+
+    public String getLastCheckedOut() {
+        return lastCheckedOut;
+    }
+
+    public String getLastCheckedOutBy() {
+        return lastCheckedOutBy;
+    }
 
     //
     public Book(String title, String author){
