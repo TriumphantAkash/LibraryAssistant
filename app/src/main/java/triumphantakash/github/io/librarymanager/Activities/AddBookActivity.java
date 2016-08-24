0fbd90a59aa4cfb87a8d8b68761b3c7516eb2f60
Akash Chaturvedi <p.akashchaturvedi@gmail.com>
2016-08-24 02:09:39 -0500

    Butterknife view injection in AddBook Activity

--- app/src/main/java/triumphantakash/github/io/librarymanager/Activities/AddBookActivity.java
+++ app/src/main/java/triumphantakash/github/io/librarymanager/Activities/AddBookActivity.java
@@ -18,6 +18,9 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 import retrofit.Callback;
 import retrofit.RestAdapter;
 import retrofit.RetrofitError;
@@ -27,14 +30,79 @@ import triumphantakash.github.io.librarymanager.models.Book;
 import triumphantakash.github.io.librarymanager.services.LibraryService;
 
 public class AddBookActivity extends AppCompatActivity {
+    @InjectView(R.id.bookTitle) EditText bookTitle;
+    @InjectView(R.id.bookAuthor) EditText bookAuthor;
+    @InjectView(R.id.bookPublisher) EditText bookPublisher;
+    @InjectView(R.id.bookCatagories) EditText bookCatagories;
 
-    EditText bookTitle, bookAuthor, bookPublisher, bookCatagories;
-    Button submitButton;
     RestAdapter restAdapter;
     String endPoint = "https://interview-api-staging.bytemark.co";
     String flag = "add";
     Book receivedBook;
     Bundle passedData;
+
+
+    @OnClick(R.id.addBookButton)
+    void onSubmitClick(){
+        receivedBook.setBookTitle(bookTitle.getText().toString());
+        receivedBook.setBookAuthor(bookAuthor.getText().toString());
+        receivedBook.setBookCatagories(bookCatagories.getText().toString());
+        receivedBook.setBookPublisher(bookPublisher.getText().toString());
+
+        if (isInputValid(receivedBook)) {
+                    /*
+                    ..
+                    ..
+                    ..
+                    */
+            //call web service POST method here and feed this book object to it
+            LibraryService libraryService = restAdapter.create(LibraryService.class);
+            if (flag.equals("add")) {
+                libraryService.addBook(receivedBook, new Callback<Object>() {
+                    @Override
+                    public void success(Object o, Response response) {
+                        Log.i("HAHA", response.toString());
+                        finish();
+                    }
+
+                    @Override
+                    public void failure(RetrofitError error) {
+                        Log.i("HAHA", "error in adding book" + error);
+                    }
+                });
+            } else {  //flag == "modify"
+                String[] urlsParts = receivedBook.getBookURL().split("/");
+                libraryService.updateBook(urlsParts[2], receivedBook, new Callback<Object>() {
+                    @Override
+                    public void success(Object o, Response response) {
+                        Log.i("HAHA", "success" + response.toString());
+                        Toast.makeText(AddBookActivity.this, "Book modified", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+
+                    @Override
+                    public void failure(RetrofitError error) {
+                        Log.i("HAHA", "error in checkout book" + error);
+                    }
+                });
+
+            }
+
+            //go back to the previous Activity
+        } else {
+            AlertDialog.Builder alert = new AlertDialog.Builder(AddBookActivity.this);
+            alert.setTitle("Incomplete Data");
+            alert.setIcon(R.drawable.warning_icon);
+            alert.setMessage("Can not add book with incomplete data. One or more fields are empty.");
+            alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    //Toast.makeText(AddBookActivity.this, "Book Deleted", Toast.LENGTH_SHORT).show();
+                }
+            });
+            alert.show();
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,82 +112,15 @@ public class AddBookActivity extends AppCompatActivity {
 
         passedData = getIntent().getExtras();
 
+        ButterKnife.inject(this);
 
         restAdapter = new RestAdapter.Builder().setEndpoint(endPoint).build();
-        bookTitle = (EditText)findViewById(R.id.bookTitle);
-        bookAuthor = (EditText)findViewById(R.id.bookAuthor);
-        bookPublisher = (EditText)findViewById(R.id.bookPublisher);
-        bookCatagories = (EditText)findViewById(R.id.bookCatagories);
-        submitButton = (Button)findViewById(R.id.addBookButton);
 
         if(passedData.getString("operation").equals("modify")){
             modifyBook();
         } else {
             receivedBook = new Book();
         }
-
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                receivedBook.setBookTitle(bookTitle.getText().toString());
-                receivedBook.setBookAuthor(bookAuthor.getText().toString());
-                receivedBook.setBookCatagories(bookCatagories.getText().toString());
-                receivedBook.setBookPublisher(bookPublisher.getText().toString());
-
-                if (isInputValid(receivedBook)) {
-                    /*
-                    ..
-                    ..
-                    ..
-                    */
-                    //call web service POST method here and feed this book object to it
-                    LibraryService libraryService = restAdapter.create(LibraryService.class);
-                    if (flag.equals("add")) {
-                        libraryService.addBook(receivedBook, new Callback<Object>() {
-                            @Override
-                            public void success(Object o, Response response) {
-                                Log.i("HAHA", response.toString());
-                                finish();
-                            }
-
-                            @Override
-                            public void failure(RetrofitError error) {
-                                Log.i("HAHA", "error in adding book" + error);
-                            }
-                        });
-                    } else {  //flag == "modify"
-                        String[] urlsParts = receivedBook.getBookURL().split("/");
-                        libraryService.updateBook(urlsParts[2], receivedBook, new Callback<Object>() {
-                            @Override
-                            public void success(Object o, Response response) {
-                                Log.i("HAHA", "success" + response.toString());
-                                Toast.makeText(AddBookActivity.this, "Book modified", Toast.LENGTH_SHORT).show();
-                                finish();
-                            }
-
-                            @Override
-                            public void failure(RetrofitError error) {
-                                Log.i("HAHA", "error in checkout book" + error);
-                            }
-                        });
-
-                    }
-
-                    //go back to the previous Activity
-                } else {
-                    AlertDialog.Builder alert = new AlertDialog.Builder(AddBookActivity.this);
-                    alert.setTitle("Incomplete Data");
-                    alert.setIcon(R.drawable.warning_icon);
-                    alert.setMessage("Can not add book with incomplete data. One or more fields are empty.");
-                    alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int whichButton) {
-                            //Toast.makeText(AddBookActivity.this, "Book Deleted", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                    alert.show();
-                }
-            }
-        });
 
     }
 
